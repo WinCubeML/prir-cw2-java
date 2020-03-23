68c31e95ff97445b42b8aa6c59fc89e9fb9a068e
luczamat <Mateusz.Luczak@orange.com>
2020-03-23 19:55:36 +0100

    Thread classes init

--- src/main/FrobeniusThread.java
+++ src/main/FrobeniusThread.java
@@ -1,6 +1,11 @@
 package main;
 
 public class FrobeniusThread implements Runnable {
+  private Matrix matrix;
+  private int thread;
+  private Pair<Integer, Integer> range;
+  private double[] result;
+
   @Override
   public void run() {
 
--- src/main/MultiplicationThread.java
+++ src/main/MultiplicationThread.java
@@ -1,6 +1,11 @@
 package main;
 
 public class MultiplicationThread implements Runnable {
+  private Matrix matrixA;
+  private Matrix matrixB;
+  private Matrix resultMatrix;
+  private Pair<Integer, Integer> range;
+
   @Override
   public void run() {
 
