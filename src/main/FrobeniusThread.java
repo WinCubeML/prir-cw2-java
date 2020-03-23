90a9aa57d1ee5144b8dc4688c5881cd1a9d96c79
luczamat <Mateusz.Luczak@orange.com>
2020-03-23 21:19:39 +0100

    Frobenius Thread

--- src/main/FrobeniusThread.java
+++ src/main/FrobeniusThread.java
@@ -4,17 +4,22 @@ public class FrobeniusThread implements Runnable {
   private Matrix matrix;
   private int thread;
   private Pair<Integer, Integer> range;
-  private double[] result;
+  private double[] resultArray;
 
-  public FrobeniusThread(Matrix matrix, int thread, Pair<Integer, Integer> range, double[] result) {
+  public FrobeniusThread(Matrix matrix, int thread, Pair<Integer, Integer> range, double[] resultArray) {
     this.matrix = matrix;
     this.thread = thread;
     this.range = range;
-    this.result = result;
+    this.resultArray = resultArray;
   }
 
   @Override
   public void run() {
-
+    double sum = 0;
+    for (int index = range.getFirst(); index < range.getSecond(); index++) {
+      Pair<Integer, Integer> rowAndColumn = matrix.mapIndexToRowAndColumn(index);
+      sum += Math.pow(matrix.get(rowAndColumn.getFirst(), rowAndColumn.getSecond()), 2);
+    }
+    resultArray[thread] = sum;
   }
 }
